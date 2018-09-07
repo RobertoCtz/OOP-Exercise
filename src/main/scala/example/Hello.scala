e7065a4b3a4836db4f183b192ed22861da11bdb0
Roberto Cortez <rcortez@intersysconsulting.com>
2018-09-07 12:45:50 -0500

    Overrided getFibonacci method of iCalculator in iCalculator 2. Changed the fib method from private to protected in order to have access to it from iCalculator2 class when inherting the class

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -2,27 +2,30 @@ package example
 
 object exercise {
   def main(args: Array[String]): Unit = {
-    
+
     val calculator = new iCalculator()
     println(calculator getFibonacci)
 
     val calculator2 = new iCalculator2()
     println(calculator2 add)
     println(calculator2 subtract)
+    println(calculator2 getFibonacci)
+
   }
 }
 
 
 class iCalculator() {
   //Recursive function that calculates fibonacci from a list
-  private def fib: Stream[Int] = 0 #:: fib.scanLeft(1)(_ + _)
+  protected def fib: Stream[Int] = 0 #:: fib.scanLeft(1)(_ + _)
 
   //Calls fib method on a 10 element list and gets the fibonacci list until
   //the 10th element
   def getFibonacci: List[Int] = fib take 10 toList
 }
 
-class iCalculator2() extends iCalculator(){
+class iCalculator2() extends iCalculator{
   def add: String = "Crystian Carranco"
   def subtract: String =  "Homero Silva"
+  override def getFibonacci: List[Int] = fib take 20 toList
 }
