97b55f68d5de4c50a2954c3504b421d7bb104f09
Roberto Cortez <rcortez@intersysconsulting.com>
2018-09-07 12:13:45 -0500

    Corrected iCalculator function to have only one private and one public methods

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -13,7 +13,8 @@ class iCalculator() {
   //Recursive function that calculates fibonacci from a list
   private def fib: Stream[Int] = 0 #:: fib.scanLeft(1)(_ + _)
 
-  private def fibonacci: List[Int] = fib take 10 toList
+  //Calls fib method on a 10 element list and gets the fibonacci list until
+  //the 10th element
+  def getFibonacci: List[Int] = fib take 10 toList
 
-  def getFibonacci(): List[Int] =  fibonacci
 }
